186c61f08420e996a37b00e8225d16db9c28a68f
Cheng0Xin <71752581+Cheng0Xin@users.noreply.github.com>
2024-01-06 16:09:16 +0100

    R.

--- wezterm.lua
+++ wezterm.lua
@@ -1,43 +1,43 @@
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
 
 local config = {}
 
 if wezterm.config_builder then
-  config = wezterm.config_builder()
+	config = wezterm.config_builder()
 end
 
 -- Colorscheme
-local my_colorscheme = require 'colorscheme'
+local my_colorscheme = require("colorscheme")
 config.color_scheme = my_colorscheme.color_scheme
 
 -- For macos: Native fullscreen
--- config.native_macos_fullscreen_mode = true
+config.native_macos_fullscreen_mode = true
 config.window_decorations = "RESIZE"
 config.hide_tab_bar_if_only_one_tab = true
 config.window_padding = {
-  left = 4,
-  right = 0,
-  top = 10,
-  bottom = 0,
+	left = 4,
+	right = 0,
+	top = 10,
+	bottom = 0,
 }
 config.enable_scroll_bar = false
 
 -- Font
-config.font = wezterm.font_with_fallback {
-  'DejaVuSansMono Nerd Font Mono',
-  'BlexMono Nerd Font Propo',
-}
+config.font = wezterm.font_with_fallback({
+	"DejaVuSansMono Nerd Font Mono",
+	"BlexMono Nerd Font Propo",
+})
 config.font_size = 18
 
 -- Tab Theme
-local my_color = require 'tabtheme'
+local my_color = require("tabtheme")
 config.window_frame = my_color.window_frame
 config.colors = my_color.colors
 config.tab_bar_at_bottom = my_color.tab_bar_at_bottom
 config.use_fancy_tab_bar = my_color.use_fancy_tab_bar
 
 -- Keybinding
-local my_keybinding = require 'keybinding'
+local my_keybinding = require("keybinding")
 my_keybinding.configure_key(config)
 
 return config
